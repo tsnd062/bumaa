5373c5c09924255f2ad868ebb7bae02dd110a314
tsnd062 <mikasanna65@gmail.com>
2024-10-25 16:18:10 +0800

    Add files via upload

---
+++ shalgalt.py
@@ -0,0 +1,23 @@
+
+# 1) x - Integer, y - Float, z - String
+# 2)012 
+# 3)c
+# 4)d
+# 5)c
+# 6)a
+# 7)a
+# 8)c
+# 9)a
+# 10)d
+# 11)b
+# 12)a
+# 14)stack zowhn neg daraallaar (lifo zarcim) 
+# tree sgatalsn olon salaalsn butesttei 
+# 15)c
+# 16)a
+# 17)b
+# 18)a
+# 19)b 
+# 20)b
+# 21)b
+# 22)d
